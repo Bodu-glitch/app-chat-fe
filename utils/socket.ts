dda90f5d7342e8739ab6ff2b23759e336a0ec2dd
Bo <tn212079@gmail.com>
2026-01-22 04:16:36 +0700

    f

--- utils/socket.ts
+++ utils/socket.ts
@@ -1,6 +1,6 @@
 import {io} from 'socket.io-client';
 
-export const socket = io('http://localhost:80/chat', {
+export const socket = io('http://localhost:81/chat', {
     autoConnect: true,
     withCredentials: false,
     reconnectionAttempts: Infinity,
